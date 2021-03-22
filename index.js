241314198eb5f6a13604f70e481f97d6fc6161b9
vanessuniq <vanessuniq@gmail.com>
2021-03-21 22:38:20 -0400

    commit tree a63eeedb6d90fdea0c8a166ce27ea1437b5a4072

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 function largestSubarraySum(array) {
     // code to write here
-    function sum(total, num) {
-        return total + num;
-    }
-    let start = 1,
-        end = array.length - 1,
-        max = array.reduce(sum, 0);
-    while (start !== end) {
-        const max1 = array.slice(start).reduce(sum, 0);
-        const max2 = array.slice(0, end).reduce(sum, 0);
-        const max3 = array.slice(start - 1, end + 1).reduce(sum, 0);
-        max = Math.max(max, max1, max2, max3);
-
-        start++;
-        end--;
+    let currentSum = 0,
+        max = 0;
+    for (const num of array) {
+        currentSum = Math.max(0, currentSum + num);
+        max = Math.max(currentSum, max);
     };
     return max;
 }
